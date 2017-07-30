d7bec082d9f20b240e892ef81eab72c22432e2c0
Mehul <mehul9595@gmail.com>
2017-07-30 20:43:01 +0800

    change server credentials

--- src/main/java/com/tinywebgears/relayme/serverside/ProductionEnvironment.java
+++ src/main/java/com/tinywebgears/relayme/serverside/ProductionEnvironment.java
@@ -3,7 +3,7 @@ package com.tinywebgears.relayme.serverside;
 public class ProductionEnvironment {
     // FIXME: 000: Update this with your own environment's details
     private static final String SERVER_SIDE_URL_PRODUCTION =
-            "https://relay-me-test-server-side.herokuapp.com/relayme/server";
+            "https://relayme-test-mehul.herokuapp.com/relayme/server";
 
     public String getServerBaseUrl() {
         return SERVER_SIDE_URL_PRODUCTION;
